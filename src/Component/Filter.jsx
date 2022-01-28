e49cf3270c1daa8e8da5c7c40b9cb2c927aa9614
dat.truong <truongquocdat196nt@gmail.com>
2022-01-28 08:27:25 +0700

    feat: filter-region

---
+++ src/Component/Filter.jsx
@@ -0,0 +1,28 @@
+const genFilterItems = (items) => {
+	return items.map((item) => (
+		<li key={item.region} className="filter__option-item">
+			<input type="checkbox" id={`region__${item.region}`} className="filter__option-checkbox" onChange={() => {console.log(items); item.checked = !item.checked}} />
+			<label htmlFor={`region__${item.region}`} className="filter__option-label">{item.region}</label>
+		</li>
+	))
+}
+
+export const Filter = () => {
+	const items = [
+		{checked: false, region: "Asia"},
+		{checked: false, region: "Europe"},
+		{checked: false, region: "Africa"},
+		{checked: false, region: "Autralia"},
+	]
+
+	return (
+		<div className="filter__container">
+			<input type="checkbox" className="filter__dropdown-checkbox" id="filter__dropdown" />
+			<label htmlFor="filter__dropdown" className="filter__dropdown-text">Filter by Region</label>
+			<label htmlFor="filter__dropdown" className="filter__dropdown-icon"></label>
+			<ul className="filter__option">
+				{genFilterItems(items, (item) => console.log(item))}
+			</ul>
+		</div>
+	)
+}
